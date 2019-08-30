ba7a062a0a2075037f8ade1bd4fdc56a43529870
Cathy Chen <chencfb@bc.edu>
2019-08-29 20:45:09 -0400

    🐗🐗饿了button if/else

--- Two-Button/ViewController.swift
+++ Two-Button/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func button(_ sender: Any) {
+        if label.text == "🐗🐗饿了！"{
+            label.text = "🐗要吃饭！"
+        }
+        else {
+            label.text = "🐗🐗饿了！"
+        }
+    }
+    
 }
 
